485c79b9e6bf414e4264c6c2ff529a07c4f3431b
Nicholas.Arocho <nick@boldlyfine.com>
2020-09-10 19:06:56 -0700

    set up prisma and graphql boiler plate

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,10 @@
+// import React from 'react'; //NOTE: don't need this cuz Next.js takes care of that for us
+import Link from 'next/link';
+
+const Home = props => (
+  <div>
+    <p>Home!</p>
+  </div>
+)
+
+export default Home;
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,10 @@
+// import React from 'react'; //NOTE: don't need this cuz Next.js takes care of that for us
+import Link from 'next/link';
+
+const Sell = props => (
+  <div>
+    <p>Sell!</p>
+  </div>
+)
+
+export default Sell;
